bbc5036a134ad951e64ce851779e2aaea951b7d7
QuenMBar <Qbeast360@gmail.com>
2020-02-14 15:58:21 +0000

    -2

---
+++ lab02/program1.c
@@ -0,0 +1,10 @@
+/* My first Raspberry Pi program
+ Name: your name
+*/
+#include <stdio.h>
+int main(void)
+{
+ printf("Hello world.\n");
+ printf("Compiled and run on a Raspberry Pi.\n");
+ return 0;
+}
